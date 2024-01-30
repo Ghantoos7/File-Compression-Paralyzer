14aa09c2c54d013b909c3a01c4ac9a6e73625104
Ghantoos7 <ghnatios1@gmail.com>
2024-01-30 19:18:34 +0200

    <Main> Added function to parralize files by the number of cores

--- compression.c
+++ compression.c
@@ -36,11 +36,56 @@ int parrarel_compression_all(const char *folderPath)
 
 
 
+int parallel_compression_cores(const char *folderPath, int NB_CORES)
+{
+    int filesCount = countFiles(folderPath);
+    char **filesList = listFiles(folderPath);
+
+    int T1 = time(NULL);
+
+    for (int i = 0; i < filesCount; i += NB_CORES)
+    {
+        int end = i + NB_CORES;
+        if (end > filesCount)
+        {
+            end = filesCount;
+        }
+
+        for (int j = i; j < end; j++)
+        {
+            int value = fork();
+
+            if (value == -1)
+            {
+                printf("Error\n");
+                exit(1);
+            }
+            else if (value == 0)
+            {
+                char command[256];
+                sprintf(command, "gzip -k -f %s", filesList[j]);
+                system(command);
+                exit(0);
+            }
+        }
+
+        for (int j = i; j < end; j++)
+        {
+            wait(NULL);
+        }
+    }
+
+    int T2 = time(NULL);
+    return T2 - T1;
+}
+
+
+
 int main(int argc, char **argv)
 {
 
     removeGzFiles(argv[1]);
-    
+
     printf("it took %d seconds to compres all those files (parralizing all)\n" , parrarel_compression_all(argv[1]));
 
    
